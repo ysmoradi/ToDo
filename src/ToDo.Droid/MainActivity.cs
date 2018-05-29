2b348a555f24b4f2ff9778b48d027cd43e6bd446
Yaser Moradi <ysmoradi@outlook.com>
2018-05-29 18:21:00 +0430

    Enhanced keyboard management in android version

--- src/ToDo.Droid/MainActivity.cs
+++ src/ToDo.Droid/MainActivity.cs
@@ -9,6 +9,7 @@ using Prism;
 using Prism.Ioc;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 
 namespace ToDo.Droid
 {
@@ -31,6 +32,9 @@ namespace ToDo.Droid
             ImageCircleRenderer.Init();
 
             LoadApplication(new App(new ToDoInitializer()));
+
+            Xamarin.Forms.Application.Current.On<Xamarin.Forms.PlatformConfiguration.Android>()
+                .UseWindowSoftInputModeAdjust(WindowSoftInputModeAdjust.Resize);
         }
     }
 
